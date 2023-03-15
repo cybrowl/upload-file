bba515dddd4f8239f8f8291f4e82ad2104adcdf6
cyberowl <cybrowl@protonmail.com>
2023-03-15 02:12:52 -0700

    remove: logs

--- tests/file_scaling_manager.test.cjs
+++ tests/file_scaling_manager.test.cjs
@@ -153,8 +153,6 @@ test("FileStorage[motoko].commit_batch(): should start formation of asset to be 
 test("FileStorage[motoko].assets_list(): should return all assets without file content data since it would be too large", async function (t) {
   const { ok: asset_list } = await file_storage_actors.motoko.assets_list();
 
-  console.log("asset_list: ", asset_list);
-
   const hasAssets = asset_list.length > 0;
 
   t.equal(hasAssets, true);
@@ -163,8 +161,6 @@ test("FileStorage[motoko].assets_list(): should return all assets without file c
 test("FileScalingManager[motoko].get_canister_records(): should return all canister records", async function (t) {
   const records =
     await file_scaling_manager_actors.motoko.get_canister_records();
-
-  console.log("records: ", records);
 
   const hasAssets = records.length > 0;
 
